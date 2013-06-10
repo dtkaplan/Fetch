0f5a33aa2f1f9e15e03a234eab98a20663a76d83
aaronmayerson <aamayerson@gmail.com>
2013-06-10 12:10:29 -0500

    test commit

--- R/Fetch.R
+++ R/Fetch.R
@@ -263,7 +263,6 @@ fetchDat <- function(name,pkg = NULL){
   else return(TRUE)
 }
 
-####  Change
 .getText <- function(name){
   if (suppressWarnings((require(RCurl,quietly=TRUE)))){
     if (url.exists(name)){
